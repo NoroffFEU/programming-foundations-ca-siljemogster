cb6acd6e6fdef8e1120013f0f6b6cdbf406a9f4b
siljemogster <125352923+siljemogster@users.noreply.github.com>
2023-10-21 21:19:22 +0200

    updates in readme

--- camperVan.js
+++ camperVan.js
@@ -6,7 +6,7 @@ const camperVan = {
   color: "blue",
   isOld: true,
   numberofWheels: 4,
-  datesOfService: [1988, 1990, 1992, 1994, 1996, 1998],
+  datesOfService: [1988, 1990, 1992, 1994, 1996, 1998, 2000],
   printDatesOfService: function () {
     this.datesOfService.forEach(function (serviceDates) {
       console.log(serviceDates);
